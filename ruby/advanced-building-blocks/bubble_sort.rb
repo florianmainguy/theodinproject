4262cc0bf5cefc114ad2e4afb857a5ee353380c0
florianmainguy <florian.mainguy@gmail.com>
2015-08-21 15:33:28 +0100

    bubble sort by

--- ruby/advanced-building-blocks/bubble_sort.rb
+++ ruby/advanced-building-blocks/bubble_sort.rb
@@ -30,4 +30,29 @@ def bubble_sort(array)
   puts array.inspect
 end
 
+def bubble_sort_by(array)
+  no_pb = false
+  while !no_pb
+    no_pb = true
+    array.each_with_index do |value,index|
+      unless index == array.length-1
+        if yield(array[index],array[index+1]) <= 0 
+		  temp1=array[index]
+		  temp2=array[index+1]
+        else
+		  temp1=array[index+1]
+		  temp2=array[index]
+		  no_pb = false
+        end
+  	    array[index]=temp1
+	    array[index+1]=temp2
+      end
+    end
+  end
+  puts array.inspect
+end
+
 bubble_sort([4,3,78,2,0,2])
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
