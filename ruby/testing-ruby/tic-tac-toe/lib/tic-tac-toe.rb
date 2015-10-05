4c722bbf9dc8ab439f6cc22de443b2c20f55863a
florianmainguy <florian.mainguy@gmail.com>
2015-10-05 20:32:32 +0100

    Rspec tic tac toe solved

--- ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
+++ ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
@@ -1,4 +1,7 @@
-# Tic Tac Toe
+# This Rspec excercise made me changed my Tic Tac Toe program.
+
+# I improved the fact that each method should have its own purpose. It is much easier
+# to test this way. And it's easier to read throught.
 
 class Player
   attr_reader :name
@@ -19,36 +22,34 @@ class TicTacToe
   		         :c1 => " ", :c2 => " ", :c3 => " " }
   end
 
-  # Isolated ;ethod for TDD
-  # Returns @table
-  def get_board
-    @table
-  end
-
+  # Displays Tic Tac Toe board
   def display
-    @table = get_board
   	puts "     A   B   C"
     puts "   -------------"
-    puts " 1 | " + @table[:a1] + " | " + @table[:b1] + " | " + @table[:c1] + " |"
+    puts " 1 | " + board(:a1) + " | " + board(:b1) + " | " + board(:c1) + " |"
   	puts "   -------------"
-  	puts " 2 | " + @table[:a2] + " | " + @table[:b2] + " | " + @table[:c2] + " |"
+  	puts " 2 | " + board(:a2) + " | " + board(:b2) + " | " + board(:c2) + " |"
   	puts "   -------------"
-  	puts " 3 | " + @table[:a3] + " | " + @table[:b3] + " | " + @table[:c3] + " |"
+  	puts " 3 | " + board(:a3) + " | " + board(:b3) + " | " + board(:c3) + " |"
   	puts "   -------------"
   end  
 
-  # Isolated method for TDD
-  # Returns bool
-  def case_ok(bool = false)
-    bool
+  # Asks the player to select a case and returns it
+  def case_chosen(player)
+    puts"You turn, " + player.name.capitalize + ". Select a case:"
+    loop do
+      case_selected = gets.chomp.downcase
+      return case_selected.to_sym if handle_selection(case_selected)
+    end
   end
 
+  # Checks that the selected case is correct and can be played
   def handle_selection(input)
-    cases = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
+    cases = ['a1','a2','a3','b1','b2','b3','c1','c2','c3']
 
-    if !cases.include?(case_selected)
+    if !cases.include?(input)
       puts "Sorry I didn't understand. Which case ? (ex: a2, c3, b1)"
-    elsif @table[case_selected.to_sym] != " "
+    elsif board(input.to_sym) != " "
       puts "Case already played ! Select another one:"
     else
       return true
@@ -56,36 +57,29 @@ class TicTacToe
     return false
   end
 
-  def case_chosen(player)
-    @table = get_board
-  	
-  	bool = case_ok
-
-    puts"You turn, " + player.name.capitalize + ". Select a case:"
-    loop do
-      case_selected = gets.chomp.downcase
-      break if handle_selection(case_selected)
+  # Fills or displays only a board case
+  def board(case_board, player_symbol = nil)
+    if player_symbol
+      @table[case_board] = player_symbol
+    else
+      @table[case_board]
     end
   end
 
-  def fill_case(case_to_fill, player_symbol)
-    @table = get_board
-  	@table[case_to_fill.to_sym] = player_symbol
-  end
-
+  # Checks if a player won
   def victory?(sign)
-    @table = get_board
   	result = false
     win = [[:a1,:a2,:a3],[:b1,:b2,:b3],[:c1,:c2,:c3],[:a1,:b1,:c1],
            [:a2,:b2,:c2],[:a3,:b3,:c3],[:a1,:b2,:c3],[:a3,:b2,:c1]]
     win.each do |a,b,c|
-      if @table[a] == sign && @table[b] == sign && @table[c] == sign
+      if board(a) == sign && board(b) == sign && board(c) == sign
         result = true
       end
     end
     result
   end
 
+  # Launches the game
   def start
     puts "What's the name of Player 1 ?"
     player1 = Player.new(gets.chomp.downcase,1,"X")
@@ -99,7 +93,7 @@ class TicTacToe
     victory = false
     until stage == 10 || winner
       display
-      fill_case(case_chosen(player),player.symbol)
+      board(case_chosen(player),player.symbol)
       winner = player.name if victory?(player.symbol)
       player == player1 ? player = player2 : player = player1
       stage += 1
--- ruby/testing-ruby/tic-tac-toe/spec/tic-tac-toe_spec.rb
+++ ruby/testing-ruby/tic-tac-toe/spec/tic-tac-toe_spec.rb
@@ -1,62 +1,68 @@
+# This Rspec excercise made me changed my Tic Tac Toe program.
+
+# I improved the fact that each method should have its own purpose. It is much easier
+# to test this way. And it's easier to read throught.
+
 require './lib/tic-tac-toe.rb'
 
 describe TicTacToe do
   
   before :each do
     @game = TicTacToe.new
-    @player = Player.new('flo', 0, 'X')
+    @player1 = Player.new('flo', 0, 'X')
+    @player2 = Player.new('flo', 0, 'O')
   end
   
   describe '#case_chosen' do
     it 'returns the case selected' do 
-      @game.stub(:get_board).and_return({ :a1 => "X", :a2 => "O", :a3 => "X",
-                                          :b1 => " ", :b2 => "O", :b3 => " ",
-                                          :c1 => " ", :c2 => "X", :c3 => " " })
-      @game.stub(:select) {'c1'}
-      @game.case_chosen(@player).should eql 'c1'
+      @game.stub(:gets).and_return('c1')
+      expect(@game.case_chosen(@player1)).to eql :c1
+    end
+  end
+
+  describe '#handle_selection' do
+    it 'returns true if the case selected is available' do
+      expect(@game.handle_selection('c1')).to be_true
     end
 
-    it 'asks the player to select again if case already taken' do
-      @game.stub(:get_board).and_return({ :a1 => "X", :a2 => "O", :a3 => "X",
-                                          :b1 => " ", :b2 => "O", :b3 => " ",
-                                          :c1 => " ", :c2 => "X", :c3 => " " })
-      @game.stub(:select) {'a1'}
-      @game.stub(:case_ok) {true}
-      @game.case_chosen(@player)
-      expect(@game).to receive(:puts).with('Case already played ! Select another one:')
+    it 'returns false if the case selected is already taken' do
+      @game.board(:c1, @player1.symbol)
+      expect(@game.handle_selection('c1')).to be_false
     end
 
-    it 'asks the player to select again if player gave wrong input' do
-      @game.stub(:get_board).and_return({ :a1 => "X", :a2 => "O", :a3 => "X",
-                                          :b1 => " ", :b2 => "O", :b3 => " ",
-                                          :c1 => " ", :c2 => "X", :c3 => " " })
-      @game.stub(:select) {'potato'}
-      @game.stub(:case_ok) {true}
-      @game.case_chosen(@player)
-      expect(@game.case_chosen(@player)).to receive(:puts).with("Sorry I didn't understand. Which case ? (ex: a2, c3, b1)")
+    it 'returns false if the case selected is not a case' do
+      expect(@game.handle_selection('potato')).to be_false
+    end
+  end
+
+  describe '#board' do
+    it 'reads the case passed if only argument' do
+      expect(@game.board(:b3)).to eql ' '
+    end
+
+    it 'fills the case passed by the player symbol' do
+      @game.board(:b2, @player1.symbol)
+      expect(@game.board(:b2)).to eql @player1.symbol
     end
   end
 
   describe '#victory' do
     it 'returns true for 3 X at the top' do
-      @game.stub(:get_board).and_return({ :a1 => "X", :a2 => "X", :a3 => "X",
-                                          :b1 => " ", :b2 => " ", :b3 => " ",
-                                          :c1 => " ", :c2 => " ", :c3 => " " })
-      @game.victory?("X").should eql true
+      @game.board(:a1, @player1.symbol)
+      @game.board(:b1, @player1.symbol)
+      @game.board(:c1, @player1.symbol)
+      expect(@game.victory?('X')).to be_true
     end
 
     it 'returns true for 3 O in diagonal' do
-      @game.stub(:get_board).and_return({ :a1 => " ", :a2 => " ", :a3 => "O",
-                                          :b1 => " ", :b2 => "O", :b3 => " ",
-                                          :c1 => "O", :c2 => " ", :c3 => " " })
-      @game.victory?("O").should eql true
+      @game.board(:a3, @player2.symbol)
+      @game.board(:b2, @player2.symbol)
+      @game.board(:c1, @player2.symbol)
+      expect(@game.victory?('O')).to be_true
     end
 
-    it 'returns false if a player didnt win and the board is full' do
-      @game.stub(:get_board).and_return({ :a1 => "X", :a2 => "O", :a3 => "X",
-                                          :b1 => "O", :b2 => "X", :b3 => "X",
-                                          :c1 => "O", :c2 => "X", :c3 => "O" })
-      @game.victory?("O").should eql false
+    it 'returns false if a player didnt win' do
+      expect(@game.victory?('O')).to be_false
     end
   end
 end
