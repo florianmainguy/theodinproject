ba13edac9195f75341d059b735a1b7aa902ad9b3
florianmainguy <florian.mainguy@gmail.com>
2015-10-05 20:40:25 +0100

    Update tic-tac-toe.rb

--- ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
+++ ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
@@ -17,9 +17,9 @@ end
 
 class TicTacToe
   def initialize
-  	@table = { :a1 => " ", :a2 => " ", :a3 => " ",
-  		         :b1 => " ", :b2 => " ", :b3 => " ",
-  		         :c1 => " ", :c2 => " ", :c3 => " " }
+    @table = { :a1 => " ", :a2 => " ", :a3 => " ",
+               :b1 => " ", :b2 => " ", :b3 => " ",
+               :c1 => " ", :c2 => " ", :c3 => " " }
   end
 
   # Displays Tic Tac Toe board
