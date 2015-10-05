0158d97faf6b6e17d2490060318f29260e088cb6
florianmainguy <florian.mainguy@gmail.com>
2015-10-05 20:41:34 +0100

    Update tic-tac-toe.rb

--- ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
+++ ruby/testing-ruby/tic-tac-toe/lib/tic-tac-toe.rb
@@ -68,7 +68,7 @@ class TicTacToe
 
   # Checks if a player won
   def victory?(sign)
-  	result = false
+    result = false
     win = [[:a1,:a2,:a3],[:b1,:b2,:b3],[:c1,:c2,:c3],[:a1,:b1,:c1],
            [:a2,:b2,:c2],[:a3,:b3,:c3],[:a1,:b2,:c3],[:a3,:b2,:c1]]
     win.each do |a,b,c|
