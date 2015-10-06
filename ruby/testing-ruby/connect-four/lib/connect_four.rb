2f2bc9c7fcbca20e6079697b0c5042833cbcbf5b
florianmainguy <florian.mainguy@gmail.com>
2015-10-06 21:50:05 +0100

    bug  if check vertical

--- ruby/testing-ruby/connect-four/lib/connect_four.rb
+++ ruby/testing-ruby/connect-four/lib/connect_four.rb
@@ -3,6 +3,8 @@
 # The objective of the game is to connect four of one's own discs of the same color
 # next to each other vertically, horizontally, or diagonally.
 
+
+
 class Player
   attr_reader :name, :color
 
@@ -12,6 +14,8 @@ class Player
   end
 end
 
+
+
 class CellNode
   attr_accessor :color, :left, :up, :right, :down, :diag_lu, :diag_ld, :diag_ru, :diag_rd
 
@@ -20,18 +24,140 @@ class CellNode
   end
 end
 
+
+
 class Board
   attr_reader :grid
+  attr_accessor :last_cell_played
 
-  def initialize
-    @grid = default_grid
+  def initialize(grid = nil)
+    @grid = grid || default_grid
+    connect_cells #if grid.nil?
   end
 
+  # Returns cell at coordinate (x, y)
+  def get_cell(x, y)
+    grid[x][y]
+  end
+
+  # Sets color of cell (x, y)
+  def set_cell(x, y, color)
+    get_cell(x, y).color = color
+    last_cell_played = get_cell(x, y)
+  end
+
+  # Returns :winner if there is a winner, :draw if there is a draw,
+  # or false if the game is still being played
+  def game_over
+    return :winner if winner?
+    return :draw if draw?
+    false
+  end
+
+  private
+
+  # Sets a default grid of 7 columns and 7 rows
   def default_grid
+    Array.new(7) { Array.new(6) { CellNode.new }}
   end
 
+  # Links cells between them
+  def connect_cells
+    @grid.each_with_index do |columns, columns_index|
+      columns.each_with_index do |cell, cell_index|
+        cell.left    = @grid[columns_index - 1][cell_index]     || nil if @grid[columns_index - 1]
+        cell.up      = @grid[columns_index][cell_index + 1]     || nil if @grid[columns_index]
+        cell.right   = @grid[columns_index + 1][cell_index]     || nil if @grid[columns_index + 1]
+        cell.down    = @grid[columns_index][cell_index - 1]     || nil if @grid[columns_index]
+        cell.diag_lu = @grid[columns_index - 1][cell_index + 1] || nil if @grid[columns_index - 1]
+        cell.diag_ld = @grid[columns_index - 1][cell_index - 1] || nil if @grid[columns_index - 1]
+        cell.diag_ru = @grid[columns_index + 1][cell_index + 1] || nil if @grid[columns_index + 1]
+        cell.diag_rd = @grid[columns_index + 1][cell_index - 1] || nil if @grid[columns_index + 1]
+      end
+    end
+  end
+
+  # Returns true if there is a draw
+  def draw?
+    @grid.each { |columns| columns.each { |cell| return false if cell.color == ''}}
+    return true
+  end
+
+  # Returns true if there is a winner
+  def winner?
+    return true if check_horizontal
+    return true if check_vertical
+    return true if check_diagonal1
+    return true if check_diagonal2
+    false
+  end
+
+  # Returns true if 4 same cells horizontally
+  def check_horizontal
+    cell = last_cell_played
+    count = 1
+    while cell.left && cell.left.color == cell.color
+      cell = cell.left
+    end
+    while cell.right && cell.right.color == cell.color
+      cell = cell.right
+      count += 1
+    end
+    return true if count >= 4
+    false
+  end
+
+  # Returns true if 4 same cells vertically
+  def check_vertical
+    cell = last_cell_played
+    count = 1
+    while cell.down && cell.down.color == cell.color
+      ###BUGBUGBUGBUG puts "youhou"
+      cell = cell.down
+    end
+    while cell.up && cell.up.color == cell.color
+      cell = cell.up
+      count += 1
+    end
+    return true if count >= 4
+    false
+  end
+
+  # Returns true if 4 same cells on one diagonal
+  def check_diagonal1
+    cell = last_cell_played
+    count = 1
+    while cell.diag_ld && cell.diag_ld.color == cell.color
+      cell = cell.diag_ld
+    end
+    while cell.diag_ru && cell.diag_ru.color == cell.color
+      cell = cell.diag_ru
+      count += 1
+    end
+    return true if count >= 4
+    false
+  end
+
+  # Returns true if 4 same cells on the other diagonal
+  def check_diagonal2
+    cell = last_cell_played
+    count = 1
+    while cell.diag_rd && cell.diag_rd.color == cell.color
+      cell = cell.diag_rd
+    end
+    while cell.diag_lu && cell.diag_lu.color == cell.color
+      cell = cell.diag_lu
+      count += 1
+    end
+    return true if count >= 4
+    false
+  end
 end
 
 class ConnectFour
+  def initialize
+  end
+  def game
+  end
 
 end
--- ruby/testing-ruby/connect-four/spec/connect_four_spec.rb
+++ ruby/testing-ruby/connect-four/spec/connect_four_spec.rb
@@ -1,5 +1,7 @@
 require './lib/connect_four.rb'
 
+
+
 describe Player do
   describe '#initialize' do
     it 'raises an error if called without argument' do
@@ -30,11 +32,15 @@ describe Player do
 end
 
 
+
 describe CellNode do
+  before :each do
+    @cell = CellNode.new
+  end
+
   describe '#initialize' do
     it "is initialized with a color of '' by default" do
-      cell = CellNode.new
-      expect(cell.color).to eq ''
+      expect(@cell.color).to eq ''
     end
 
     ### usefull ????? ###
@@ -42,23 +48,169 @@ describe CellNode do
       cell = CellNode.new("red")
       expect(cell.color).to eq "red"
     end
+  end
 
-    ### attr_successor gets###
-    context "#left" do
-      it "writes the" do
-        input = { color: "red", name: "Flo" }
-        player = Player.new(input)
-        expect(player.color).to eq "red"
-      end
+  context "#left" do
+    it "writes and returns 'left'" do
+      @cell.left = 'left'
+      expect(@cell.left).to eq "left"
+    end
+  end
+
+  context "#right" do
+    it "writes and returns 'right'" do
+      @cell.right = 'right'
+      expect(@cell.right).to eq "right"
+    end
+  end
+
+  context "#down" do
+    it "writes and returns 'down'" do
+      @cell.down = 'down'
+      expect(@cell.down).to eq "down"
+    end
+  end
+
+  context "#up" do
+    it "writes and returns 'up'" do
+      @cell.up = 'up'
+      expect(@cell.up).to eq "up"
+    end
+  end
+
+  context "#diag_lu" do
+    it "writes and returns 'diag_lu'" do
+      @cell.diag_lu = 'diag_lu'
+      expect(@cell.diag_lu).to eq "diag_lu"
+    end
+  end
+
+  context "#diag_ld" do
+    it "writes and returns 'diag_ld'" do
+      @cell.diag_ld = 'diag_ld'
+      expect(@cell.diag_ld).to eq "diag_ld"
+    end
+  end
+
+  context "#diag_ru" do
+    it "writes and returns 'diag_ru'" do
+      @cell.diag_ru = 'diag_ru'
+      expect(@cell.diag_ru).to eq "diag_ru"
+    end
+  end
+
+  context "#diag_rd" do
+    it "writes and returns 'diag_rd'" do
+      @cell.diag_rd = 'diag_rd'
+      expect(@cell.diag_rd).to eq "diag_rd"
     end
   end
 end
 
 
+
 describe Board do
+  before :each do
+    @board = Board.new
+  end
+
   describe '#initialize' do
     it "initializes the board" do
       expect { Board.new }.to_not raise_error
+    end
+
+    it "sets the grid with 7 columns" do
+      expect(@board.grid).to have(7).things
+    end
+
+    it "creates 6 rows in each column" do
+      @board.grid.each do |columns|
+        expect(columns).to have(6).things
+      end
+    end
+  end
+
+  context '#grid'
+    it "returns the grid" do
+    board = Board.new("blah")
+    expect(board.grid).to eq "blah"
+  end
+
+  describe 'get_cell' do
+    it "returns the cell based on the (x, y) coordinate" do
+      grid = [['', '', '', '', '', ''], ['', '', 'something', '', '', ''], 
+              ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''],
+              ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '']]
+      board = Board.new(grid)
+      expect(board.get_cell(1, 2)).to eq "something"
+    end
+  end
+
+  describe 'set_cell' do
+    it "updates the color of the cell object at (x, y) coordinate" do
+      Cat = Struct.new(:color)
+      grid = [[Cat.new('orange'), '', '', '', '', ''], ['', '', '', '', '', ''], 
+              ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''],
+              ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '']]
+      board = Board.new(grid)
+      board.set_cell(0, 0, "green")
+      expect(board.get_cell(0, 0).color).to eq "green"
+    end
+  end
+
+  describe '#game_over' do
+    #TestCell = Struct.new(:color)
+    #let(:x) { TestCell.new('X') }
+    #let(:y) { TestCell.new('Y') }
+    #let(:o) { TestCell.new }
+    let(:x) { CellNode.new('X') }
+    let(:y) { CellNode.new('Y') }
+    let(:o) { CellNode.new }
+
+    it "returns :winner if winner? is true" do
+      @board.stub(:winner?) { true }
+      expect(@board.game_over).to eq :winner
+    end
+ 
+    it "returns :draw if winner? is false and draw? is true" do
+      @board.stub(:winner?) { false }
+      @board.stub(:draw?) { true }
+      expect(@board.game_over).to eq :draw
+    end
+ 
+    it "returns false if winner? is false and draw? is false" do
+      @board.stub(:winner?) { false }
+      @board.stub(:draw?) { false }
+      expect(@board.game_over).to be_false
+    end
+
+    it "returns :draw when all spaces on the board are taken" do
+      grid = [[x, x, y, y, x, x], [y, y, x, x, y, y], [x, x, y, y, x, x], [y, y, x, x, y, y],
+              [x, x, y, y, x, x], [y, y, x, x, y, y], [x, x, y, y, x, x]]
+      board = Board.new(grid)
+      board.last_cell_played = board.get_cell(1, 2)
+      expect(board.game_over).to eq :draw
+    end
+
+    it "returns :winner when there is 4 cells of the same color horizontaly" do
+      grid = [[o, o, o, o, o, o], [o, x, x, x, x, o], [o, o, o, o, o, o], [o, o, o, o, o, o],
+              [o, o, o, o, o, o], [o, o, o, o, o, o], [o, o, o, o, o, o]]
+      board = Board.new(grid)
+      expect(board.game_over).to eq :winner
+    end
+ 
+    it "returns :winner when there is 4 cells of the same color verticaly" do
+      grid = [[o, o, o, o, o, o], [o, y, o, o, o, o], [o, y, o, o, o, o], [o, y, o, o, o, o],
+              [o, y, o, o, o, o], [o, o, o, o, o, o], [o, o, o, o, o, o]]
+      board = Board.new(grid)
+      expect(board.game_over).to eq :winner
+    end
+ 
+    it "returns :winner when there is 4 cells of the same color diagonally" do
+      grid = [[o, x, o, o, o, o], [o, o, x, o, o, o], [o, o, o, x, o, o], [o, o, o, o, x, o],
+              [o, o, o, o, o, o], [o, o, o, o, o, o], [o, o, o, o, o, o]]
+      board = Board.new(grid)
+      expect(board.game_over).to eq :winner
     end
   end
 end
