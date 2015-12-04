cc528112ddaa3bdea0985810c42f189970dc47b1
Florian Mainguy <florian.mainguy@gmail.com>
2015-12-04 11:40:08 +0000

    Chapter 11 till 11.2

--- rails/sample_app/app/models/user.rb
+++ rails/sample_app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
+  has_many :microposts, dependent: :destroy
   
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
