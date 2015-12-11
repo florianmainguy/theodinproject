c4e450c7b837561127a369f6c5562f1952046b52
Florian Mainguy <florian.mainguy@gmail.com>
2015-12-11 11:32:06 +0000

    Add user microposts

--- rails/sample_app/app/models/user.rb
+++ rails/sample_app/app/models/user.rb
@@ -39,4 +39,10 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
