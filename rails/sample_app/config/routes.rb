041989924bac240a2e1b198f5b52637f19aa0f6c
Florian Mainguy <florian.mainguy@gmail.com>
2015-12-25 17:30:31 +0000

    Add user following

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get 'login'     => 'sessions#new'
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
