bc9be0a3ba07167e4eefc373c1671e371c2dc901
florianmainguy <florian.mainguy@gmail.com>
2017-10-30 07:11:02 +0000

    Model tests for name and email

--- rails/fakebook_app/to do.rb
+++ rails/fakebook_app/to do.rb
@@ -1,4 +1,4 @@
 add signed_in_user? in application.html.erb
 add _navbar.html.erb in views/shared
 import bootstrap and gem?
-see why application.erb.html ne se lance pas
+setup user and session with tutorial and come back to error session_patj
