408d5616315a007d062febb048a4dcc9556d8747
florianmainguy <florian.mainguy@gmail.com>
2017-10-30 07:23:47 +0000

    Secure password added to User model

--- rails/fakebook_app/test/models/user_test.rb
+++ rails/fakebook_app/test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "Jean-Claude Van Damne", email: "jc@vandamne.com")
+    @user = User.new(name: "Jean-Claude Van Damne", email: "jc@vandamne.com",
+                     password: "kickboxer", password_confirmation: "kickboxer")
   end
 
   test "should be valid" do
@@ -50,5 +51,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
