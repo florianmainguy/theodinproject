bd32d9c9b801ac10566561bafe2fa69ed362c0d9
florianmainguy <florian.mainguy@gmail.com>
2017-11-11 10:20:04 +0000

    default cover image

--- rails/fakebook_app/app/uploaders/cover_uploader.rb
+++ rails/fakebook_app/app/uploaders/cover_uploader.rb
@@ -14,6 +14,10 @@ class CoverUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    "cover.png"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
--- rails/fakebook_app/app/uploaders/picture_uploader.rb
+++ rails/fakebook_app/app/uploaders/picture_uploader.rb
@@ -14,6 +14,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "cover.png"].compact.join('_'))
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
