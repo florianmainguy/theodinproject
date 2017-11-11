4ee7d6e2989308a343a7af8cb6f1505cd3e46768
florianmainguy <florian.mainguy@gmail.com>
2017-11-11 14:04:32 +0000

    Responsiveness cover image

--- rails/fakebook_app/app/uploaders/picture_uploader.rb
+++ rails/fakebook_app/app/uploaders/picture_uploader.rb
@@ -15,7 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   def default_url(*args)
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "cover.png"].compact.join('_'))
+    "missing-picture.png"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
