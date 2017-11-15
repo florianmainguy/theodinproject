86fa473b64194400bf20a4a2a73f8ea163871831
florianmainguy <florian.mainguy@gmail.com>
2017-11-15 23:36:58 +0000

    Update profile implemented

--- rails/fakebook_app/app/controllers/application_controller.rb
+++ rails/fakebook_app/app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:picture, :picture_cache, :remove_picture, 
-                                                              :cover, :cover_cache, :remove_cover])
+                                                              :cover, :cover_cache, :remove_cover,
+                                                              :first_name, :last_name, :about])
   end
 
 end
