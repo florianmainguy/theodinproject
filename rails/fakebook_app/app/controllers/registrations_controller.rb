9b4dd7db0a7cd36a852f71fb750782b4dbb49273
florianmainguy <florian.mainguy@gmail.com>
2017-11-16 16:58:51 +0000

    Search results layout updated

--- rails/fakebook_app/app/controllers/registrations_controller.rb
+++ rails/fakebook_app/app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    resource.update_with_password(params)
+    resource.update_without_password(params)
   end
 end
--- rails/fakebook_app/app/controllers/users_controller.rb
+++ rails/fakebook_app/app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
   end
 
   def photo
